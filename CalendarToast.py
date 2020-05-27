6bde2b2999054923fa60d9d29603b35e98695f24
abhijeetnigam <55867563+abhijeetnigam@users.noreply.github.com>
2020-05-27 13:20:37 +0530

    Updated file to fix the bug of calendar. restrict
    Calendar.Restrict will only pick calendar entries as per work hours settings in Outlook

--- CalendarToast.py
+++ CalendarToast.py
@@ -33,7 +33,7 @@ while 1 == 1:
 
     try:
         # Setup Date format
-        dateformat = "%m/%d/%Y"
+        dateformat = "%m/%d/%y" +" 00:00"
         # Connect to Local Outlook profile .
         outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
         calendar = outlook.GetDefaultFolder(9)  # Read the calendar folder referred to as #9 in the folder list
@@ -46,28 +46,23 @@ while 1 == 1:
         begin = today.date().strftime(dateformat)
         tomorrow = datetime.timedelta(days=1) + today
         end = tomorrow.date().strftime(dateformat)
-        print(today)
-        print(begin)
-        print(tomorrow)
-        print(end)
         appointments = appointments.Restrict("[Start] >= '" + begin + "' AND [END] <= '" + end + "'")
         now = today.time().strftime("%H:%M:%S")
-        #time.sleep(60)  # wait for 60s before checking the calendar again
+        time.sleep(60)  # wait for 60s before checking the calendar again
         # Read every appointment in the calendar
         for appointment in appointments:
-
-                #i += 1
-                #if 60 < i <= 70:
+            i += 1
+            if 60 < i <= 70:
                 # Updating Cal Log file with calendar appointments of Today
-            strmeet = appointment.start.strftime("%H:%M:%S") + appointment.Subject
-            print(strmeet)
-                #Log.callog(strmeet)
+                strmeet = appointment.start.strftime("%H:%M:%S") + appointment.Subject
+                Log.callog(strmeet)
                 # print(i,appointment.Subject)
-            #if i > 70:
-             #   i = 1
+            if i > 70:
+                i = 1
+
             if appointment.start.strftime("%H:%M:%S") <= now <= appointment.end.strftime("%H:%M:%S"):
-                strdate = datetime.datetime.strptime(appointment.end.strftime("%m/%d/%Y, %H:%M:%S"),
-                                                     "%m/%d/%Y, %H:%M:%S")
+                strdate = datetime.datetime.strptime(appointment.end.strftime("%m/%d/%y, %H:%M:%S"),
+                                                 "%m/%d/%y, %H:%M:%S")
                 meetend = strdate - today
                 meetmin = int(meetend.total_seconds() / 60)
                 if meetmin <= 10:
